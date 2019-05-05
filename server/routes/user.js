1d6f5b94da7dda636ab922c6116131dbbb9dd68f
ShyGodB <qibingfang@gamil.com>
2019-05-05 21:13:33 +0800

    change files

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,36 @@
 const Router = require('koa-router');
 const user = require('../lib/user');
+const axios = require('axios');
 const router = new Router();
+const md5 = require('md5');
 
 
 // 新增用户
 router.post("/addUser", async (ctx) => {
     const userInfo = ctx.request.body.userInfo;
-    const userTicket = ctx.request.body.userTicket;
-    const data = [userInfo.nickName, userInfo.gender, userInfo.avatarUrl, userInfo.city, userInfo.country, userInfo.province, userTicket.openid, userTicket.session_key];
-    const addUserPromise = user.addUser(data);
-    await addUserPromise;
-    ctx.body = {msg: "成功"};
+    const code = ctx.request.body.code;
+    let data;
+    const appId = 'wxd769fd7ca8895e6e';
+    const appSecret = '6a91717350ce071f0a0477d097fde800';
+    const aCode = 'authorization_code';
+    const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=${aCode}`;
+    await axios.get(url)
+        .then((response) => {
+            data = response.data;
+        })
+        .catch((error) => {
+            ctx.body = {code: '失败', msg: error}
+        });
+    const getUserPromise1 = user.getUser(data.openid);
+    const rows = await getUserPromise1;
+    if(rows.length === 0) {
+	const data1 = [userInfo.nickName, userInfo.gender, userInfo.avatarUrl, userInfo.city, userInfo.country, userInfo.province, data.openid];
+    	const addUserPromise = user.addUser(data1);
+    	await addUserPromise;
+    }
+    const getUserPromise = user.getUser(data.openid);
+    const userData = await getUserPromise;
+    ctx.body = {session_key: md5(data.session_key) ,userData: userData};
 });
 
 // 获取所有用户的openid
